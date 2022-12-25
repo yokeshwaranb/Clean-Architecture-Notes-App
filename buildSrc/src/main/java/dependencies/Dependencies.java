09878c97827c3c94565e16afffbb4638b28eef0b
yokesh7373@gmail.com <gitHub@1>
2022-12-25 22:13:54 +0530

    Added Firebase Dependency

--- buildSrc/src/main/java/dependencies/Dependencies.java
+++ buildSrc/src/main/java/dependencies/Dependencies.java
@@ -2,4 +2,8 @@ package dependencies;
 
 public class Dependencies {
     public static final String dagger = "com.google.dagger:dagger:" + Versions.dagger;
+    public static final String firebase_firestore = "com.google.firebase:firebase-firestore:" + Versions.firebase_firestore;
+    public static final String firebase_auth = "com.google.firebase:firebase-auth:" + Versions.firebase_auth;
+    public static final String firebase_analytics = "com.google.firebase:firebase-analytics:" + Versions.firebase_analytics;
+    public static final String firebase_crashlytics = "com.google.firebase:firebase-crashlytics:" + Versions.firebase_crashlytics;
 }
--- buildSrc/src/main/java/dependencies/Versions.java
+++ buildSrc/src/main/java/dependencies/Versions.java
@@ -2,4 +2,8 @@ package dependencies;
 
 public class Versions {
     public static final String dagger = "2.44.1";
+    public static final String firebase_firestore = "24.4.1";
+    public static final String firebase_analytics = "21.2.0";
+    public static final String firebase_crashlytics = "18.3.2";
+    public static final String firebase_auth = "21.1.0";
 }
